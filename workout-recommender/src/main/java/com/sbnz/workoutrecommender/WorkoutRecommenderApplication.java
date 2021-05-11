8dfc8e1d06d4ec1ecd309c808fbb5b8adffc213e
djordjeognjenovic97 <djordjeognjenovic997@gmail.com>
2021-05-11 15:43:55 +0200

    inicijalizacija spring drools backend-a

---
+++ workout-recommender/src/main/java/com/sbnz/workoutrecommender/WorkoutRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.sbnz.workoutrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutRecommenderApplication.class, args);
+	}
+
+}
---
+++ workout-recommender/src/test/java/com/sbnz/workoutrecommender/WorkoutRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbnz.workoutrecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
